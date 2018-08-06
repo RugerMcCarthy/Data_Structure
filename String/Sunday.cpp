5ecff9da01dcdec4e0bb444f0eb1285ffa285e09
Ruger <550279039@qq.com>
2018-08-06 10:13:18 +0800

    sunday

--- String/Sunday.cpp
+++ String/Sunday.cpp
@@ -24,19 +24,6 @@ int sunday(const char *text, const char *pattern) {
         i += ind[text[i + len_pat]];
     }
     return 0;
-    /*int len = strlen(pattern), len2 = strlen(text);
-    int ind[127] = {0};
-    for (int i = 0; i < 127; i++) ind[i] = len + 1;
-    for (int i = 0; pattern[i]; i++) ind[pattern[i]] = len - i;
-    for (int i = 0; i <= len2 - len;) {
-        int j = 0;
-        for (; j < len; j++) {
-            if (pattern[j] != text[i + j]) break;
-        }
-        if (j == len) return 1;
-        i += ind[text[i + len]];
-    }
-    return 0;*/
 }
 
 
